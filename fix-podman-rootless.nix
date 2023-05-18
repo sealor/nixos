95bf87ba6b2f901b5e54b9ea18ba83b75e3a851c
Stefan Richter <sealor@users.noreply.github.com>
2023-05-18 10:47:12 +0200

    Apply fix for podman only to user service

--- fix-podman-rootless.nix
+++ fix-podman-rootless.nix
@@ -1,16 +1,14 @@
 { pkgs, ... }:
 
-# see: https://discourse.nixos.org/t/how-can-i-improve-the-podman-user-service/27925
-
-let
-  my-podman = pkgs.podman.overrideAttrs (finalAttrs: previousAttrs: {
-    postPatch = ''
-      cat contrib/systemd/user/podman.service.in
-      substituteInPlace contrib/systemd/user/podman.service.in --replace KillMode=process KillMode=control-group
-      cat contrib/systemd/user/podman.service.in
-    '';
-  });
-in
 {
-  virtualisation.podman.package = my-podman;
+  # see: https://discourse.nixos.org/t/how-can-i-improve-the-podman-user-service/27925
+  systemd.packages = [
+    (pkgs.runCommandNoCC "toplevel-overrides.conf" {
+      preferLocalBuild = true;
+      allowSubstitutes = false;
+    } ''
+      mkdir -p $out/etc/systemd/user/podman.service.d/
+      echo -e "[Service]\nKillMode=control-group" > $out/etc/systemd/user/podman.service.d/overrides.conf
+    '')
+  ];
 }
