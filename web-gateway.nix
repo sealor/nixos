d582aa7077e9467b027663dd2475c67fbb152d72
Stefan Richter <sealor@users.noreply.github.com>
2023-08-09 22:03:20 +0200

    Improve Prometheus systemd unit graph

--- web-gateway.nix
+++ web-gateway.nix
@@ -4,7 +4,7 @@
   services.nginx.enable = true;
   services.nginx.virtualHosts._.locations = {
     "= /prometheus".return = "302 '/prometheus/graph?"
-      + "g0.expr=sum by(state)(node_systemd_unit_state)&g0.tab=1&g0.stacked=0&g0.show_exemplars=0&g0.range_input=4w&"
+      + "g0.expr=sum_over_time(node_systemd_unit_state{state%3D\"active\"%2Cname%3D~\".*service\"}[20m])&g0.tab=0&g0.stacked=0&g0.show_exemplars=0&g0.range_input=3h&"
       + "g1.expr=100 - (avg by (instance) (rate(node_cpu_seconds_total{job%3D\"node\"%2Cmode%3D\"idle\"}[2m])) * 100)&g1.tab=1&g1.stacked=0&g1.show_exemplars=0&g1.range_input=6h'";
 
     "/prometheus/" = {
