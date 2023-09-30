454f38e691376a89e45aa7ad0ccb3c0494499c56
Stefan Richter <sealor@users.noreply.github.com>
2023-09-30 14:45:00 +0200

    Refactor prometheus graph URL

--- prometheus/prometheus.nix
+++ prometheus/prometheus.nix
@@ -31,8 +31,10 @@ in
 
     locations = {
       "~ ^/$".return = "302 '/graph?"
-      + "g0.expr=sum_over_time(node_systemd_unit_state{state%3D\"active\"%2Cname%3D~\".*service\"}[20m])&g0.tab=0&g0.stacked=0&g0.show_exemplars=0&g0.range_input=3h&"
-      + "g1.expr=100 - (avg by (instance) (rate(node_cpu_seconds_total{job%3D\"node\"%2Cmode%3D\"idle\"}[2m])) * 100)&g1.tab=1&g1.stacked=0&g1.show_exemplars=0&g1.range_input=6h'";
+      + "g0.expr=sum_over_time(node_systemd_unit_state{state%3D\"active\"%2Cname%3D~\".*service\"}[20m])&"
+      + "g0.tab=0&g0.stacked=0&g0.show_exemplars=0&g0.range_input=3h&"
+      + "g1.expr=100 - (avg by (instance) (rate(node_cpu_seconds_total{job%3D\"node\"%2Cmode%3D\"idle\"}[2m])) * 100)&"
+      + "g1.tab=1&g1.stacked=0&g1.show_exemplars=0&g1.range_input=6h'";
 
       "~ ^/.+$" = {
         proxyPass = "http://127.0.0.1:9090";
